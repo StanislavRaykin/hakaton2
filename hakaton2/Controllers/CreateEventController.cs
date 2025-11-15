bb5ba541d042a6c6e508d7a19ec6fe9e5426f4bc
DenislavKuzev <kuzevdenis@gmail.com>
2025-11-15 14:28:06 +0200

    Login, Registration and Create Event(View)

---
+++ hakaton2/Controllers/CreateEventController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hakaton2.Controllers
+{
+    public class CreateEventController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
