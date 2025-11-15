44651e7e6b41a25c45208d73a27656c43b0324e1
stoian4o4 <stoian4o42@abv.bg>
2025-11-15 15:31:27 +0200

    Commiting Log in / Register Controller and CSHTML dani prati css ples

--- hakaton2/Controllers/EventsController.cs
+++ hakaton2/Controllers/EventsController.cs
@@ -12,7 +12,8 @@ namespace hakaton2.Controllers
             var now = DateTime.Now;
             var sample = new List<dynamic>
             {
-                new { Title = "City Cleanup", Description = "Community cleanup in downtown parks.", Start = now.AddDays(-10), End = now.AddDays(-9) },
+                new { Title = "City Cleanup1", Description = "Community cleanup in downtown parks.", Start = now.AddDays(-10), End = now.AddDays(-9) },
+                new { Title = "City Cleanup2", Description = "Community cleanup in downtown parks.", Start = now.AddDays(-7), End = now.AddDays(-6) },
                 new { Title = "Recycling Drive", Description = "Bring your recyclables - electronics accepted.", Start = now.AddDays(-1), End = now.AddDays(1) },
                 new { Title = "Tree Planting", Description = "Planting native trees along the river.", Start = now.AddDays(3), End = now.AddDays(3) }
             };
---
+++ hakaton2/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using hakaton2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hakaton2.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: /User/Login
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: /User/Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(UserLoginViewModel login)
+        {
+            if (!ModelState.IsValid)
+                return View(login);
+
+            // TODO: authenticate user against your user store
+            return RedirectToAction("Index", "Home");
+        }
+
+        // GET: /User/Register
+        [HttpGet]
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+        // POST: /User/Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(UserRegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            // TODO: create user (check uniqueness, hash password, persist)
+            TempData["SuccessMessage"] = "Успешна регистрация. Влезте в профила си.";
+            return RedirectToAction("Login");
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
